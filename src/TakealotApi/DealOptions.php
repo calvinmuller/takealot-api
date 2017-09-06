b193092d96da8866411a224edca5289730916ad2
Calvin Muller <calvinm@wigroup.co.za>
2017-09-06 13:32:19 +0200

    test with departments

---
+++ src/TakealotApi/DealOptions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: calvinmuller
+ * Date: 2017/09/06
+ * Time: 1:27 PM
+ */
+
+namespace TakealotApi;
+
+
+class DealOptions
+{
+
+
+}
---
+++ src/TakealotApi/TakealotApi.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace TakealotApi;
+
+/**
+ * Base API class, contains all code shared by the web API classes.
+ */
+use GuzzleHttp\Client;
+
+class TakealotApi extends Client
+{
+    /**
+     * The last API response executed.
+     */
+    public $client;
+
+    public function __construct(array $config = [])
+    {
+        $config = [
+            'base_uri' => getenv('TAKEALOT_API_URL', 'https://api.takealot.com/rest/v-1-5-2/'),
+        ];
+
+        parent::__construct($config);
+    }
+
+    /**
+     * Get and returns an array of common/shared headers.
+     */
+    public static function _getCommonHeaders()
+    {
+        return array(
+            'Accept' => '*/*',
+            'Content-Type' => 'application/json',
+        );
+    }
+
+
+    /**
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getDepartments()
+    {
+
+        return $this->get('types', [
+                'query' => ['nav' => 1]
+            ]
+        );
+    }
+}
